63cc428557d4066c9eea07e8747bbb71376417dd
Saadman Sakib <[Your>
2021-02-08 21:34:57 +0600

    Second commit

---
+++ app/src/main/java/com/example/fallguardian/Communicator.java
@@ -0,0 +1,40 @@
+package com.example.fallguardian;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class Communicator {
+
+    ///communication medium to client server
+    private Retrofit retrofit;
+    private Client_ACC client_acc;
+    private Client_both client_both;
+
+
+    //Tarik wifi ipV6: "https://192.168.1.103:8000/"
+    //"https://app.fakejson.com/"
+    //my phone: "http://192.168.43.180:8000/"
+    //Tarik wifi ipV4: "http://192.168.1.101:8000/"
+    private final String BaseUrl = "http://192.168.1.101:8000/";
+
+    public Communicator() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        client_acc = retrofit.create(Client_ACC.class);
+        client_both = retrofit.create(Client_both.class);
+    }
+
+    public Client_ACC getClient_acc() {
+        return client_acc;
+    }
+
+
+    public Client_both getClient_both() {
+        return client_both;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/fallguardian/LocationAndSMS.java
@@ -0,0 +1,158 @@
+package com.example.fallguardian;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
+
+class LocationAndSMS {
+    Context context;
+
+    ///regarding sms
+    boolean got_permission_to_send_sms;
+    String phoneNo, message;
+
+
+    //location information
+    String user_map_location; //[latitude],[longitude]
+    String google_map;
+    FusedLocationProviderClient fusedLocationClient;
+    Elderly elderly;
+
+    ///permissions
+    public static final int MY_PERMISSIONS_REQUEST_SEND_SMS_AND_LOCATION = 99;
+
+    public LocationAndSMS(Context c) {
+        user_map_location = "";
+        google_map = "https://maps.google.com/maps?q=";
+        got_permission_to_send_sms = false;
+        context = c;
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+    }
+
+    public void askPermissions(){
+        ///request permission for sending sms
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{Manifest.permission.SEND_SMS,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS_AND_LOCATION);
+        }
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS_AND_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ///when invoked at the start of app, donot send any sms just apply permissions
+                    if(!got_permission_to_send_sms){
+                        got_permission_to_send_sms = true;
+                    }
+                    else{
+                        sendSMSMessage();
+                    }
+                } else {
+                    Toast.makeText(context,
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+
+        }
+
+    }
+
+    protected void sendSMSMessage() {
+        try {
+            phoneNo = elderly.getMonitor_phone_number();
+            message = elderly.getFirstName()+" "+elderly.getLastName()+" fell down and might be injured"+" at location: "+user_map_location+" .";
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.SEND_SMS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions((Activity) context,
+                        new String[]{Manifest.permission.SEND_SMS,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS_AND_LOCATION);
+            }
+            else{
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                Toast.makeText(context, "SMS sent.", Toast.LENGTH_LONG).show();
+                Log.d("SensorActivty","SMS SENT ______________________________________________________________");
+            }
+
+        }
+        catch(NullPointerException e){
+            Toast.makeText(context,
+                    "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+            System.out.println("Exception Caught");
+        }
+
+    }
+
+    public void getLocationAndSendSMS(){
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION}, MY_PERMISSIONS_REQUEST_SEND_SMS_AND_LOCATION);
+        }
+        else{
+            Task<Location> locationTask = fusedLocationClient.getLastLocation();
+            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location!=null){
+                        user_map_location = google_map + location.getLatitude() + "," + location.getLongitude();
+                        Log.d("SensorActivity",user_map_location+" ********************************************************************* ");
+                        sendSMSMessage();
+
+                    }
+                    else{
+                        user_map_location = null;
+                        Log.d("SensorActivity","Location is null ********************************************************************* ");
+
+                        user_map_location = "Location unavailable";
+                        sendSMSMessage();
+                    }
+                }
+            });
+
+            locationTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("SensorActivity","Failure in location ********************************************************************* ");
+                }
+            });
+
+            locationTask.addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+
+                }
+            });
+        }
+
+    }
+
+    public void setElderly(Elderly elderly) {
+        this.elderly = elderly;
+    }
+}
